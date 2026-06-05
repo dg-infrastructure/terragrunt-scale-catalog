8466231f451fcd6966758a9811b19f679d86ce88
Robin Mavunkel <robin@gruntwork.io>
2026-06-05 11:51:15 -0700

    fixing issue when iam policy changes more than 5 times and hits version limit

--- modules/aws/iam-policy/main.tf
+++ modules/aws/iam-policy/main.tf
@@ -1,9 +1,17 @@
+locals {
+  policy_name = "${var.name}-${substr(sha256(var.policy), 0, 8)}"
+}
+
 resource "aws_iam_policy" "policy" {
-  name        = var.name
+  name        = local.policy_name
   path        = var.path
   description = var.description
 
   policy = var.policy
 
   tags = var.tags
+
+  lifecycle {
+    create_before_destroy = true
+  }
 }
